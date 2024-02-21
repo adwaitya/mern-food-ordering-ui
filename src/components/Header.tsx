8b861f50733a32c4ea0ac7bf59f56cb229ea779c
adwaitya <adwaitya.118038@gmail.com>
2024-02-21 18:39:52 +0530

    add mobile nav codes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 
 import { Link } from 'react-router-dom'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
     <div className='border-b-2 border-b-orange-500 py-6'>
         <div className='container mx-auto flex justify-between items-center'>
             <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>MernEats.com</Link>
+            <div className='md:hidden'>
+                <MobileNav/>
+            </div>
         </div>
     </div>
   )
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet'
+import { Menu } from 'lucide-react'
+import { Separator } from './ui/separator'
+import { Button } from './ui/button'
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+        <SheetTrigger>
+            <Menu className='text-orange-500'/>
+        </SheetTrigger>
+        <SheetContent className='space-y-3'>
+            <SheetTitle>
+                Welcome to MernEats.com!
+            </SheetTitle>
+            <Separator/>
+            <SheetDescription className='flex'>
+                <Button className='flex-1 font-bold bg-orange-500'>Log In</Button>
+            </SheetDescription>
+        </SheetContent>
+    </Sheet>
+  )
+}
+
+export default MobileNav
