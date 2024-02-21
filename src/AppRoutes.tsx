e227873dcdde6bdcc3d708a16a860f61f7930134
adwaitya <adwaitya.118038@gmail.com>
2024-02-21 18:22:12 +0530

    create layout and header pages

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './layouts/layout'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<span>Home Page</span>}/>
+        <Route path='/' element={<Layout><span>Home Page</span></Layout>}/>
         <Route path='/user-profile' element={<span>User Profile Page</span>}/>
         <Route path='*' element={<Navigate to='/'/>}/>
     </Routes>
