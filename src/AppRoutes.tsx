80b2f999f0394664bd4a9d095e9c562931a4b747
adwaitya <adwaitya.118038@gmail.com>
2024-02-21 23:46:15 +0530

    create landing page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layouts/layout'
+import HomePage from './pages/HomePage'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Layout><span>Home Page</span></Layout>}/>
+        <Route path='/' element={<Layout><HomePage/></Layout>}/>
         <Route path='/user-profile' element={<span>User Profile Page</span>}/>
         <Route path='*' element={<Navigate to='/'/>}/>
     </Routes>
