18d1119d55ab4dcce912833bb694610cc42b6778
adwaitya <adwaitya.118038@gmail.com>
2024-02-23 08:56:38 +0530

    Setup auth0 with ui codes

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <Button
       variant="ghost"
       className="font-bold hover:text-orange-500 hover:bg-white"
+      onClick={async () => await loginWithRedirect()}
     >
       Log In
     </Button>
