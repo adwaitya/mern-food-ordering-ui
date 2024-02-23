24bf448ac4e425e192d5008dfa175201b7419197
adwaitya <adwaitya.118038@gmail.com>
2024-02-23 09:56:03 +0530

    changed app name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className='border-b-2 border-b-orange-500 py-6'>
         <div className='container mx-auto flex justify-between items-center'>
-            <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>MernEats.com</Link>
+            <Link to='/' className='text-3xl font-bold tracking-tight text-orange-500'>Quick Bite</Link>
             <div className='md:hidden'>
                 <MobileNav/>
             </div>
