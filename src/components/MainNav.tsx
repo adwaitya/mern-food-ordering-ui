970efaea6038067143fb1641f5b02f751dbf7ce0
adwaitya <adwaitya.118038@gmail.com>
2024-02-29 17:46:18 +0530

    fixed build issue

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 import UserNameMenu from "./UserNameMenu";
--- src/pages/AuthCallBackPage.tsx
+++ src/pages/AuthCallBackPage.tsx
@@ -1,6 +1,6 @@
 import { useCreateUser } from "@/api/UserApi";
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect, useRef } from "react";
+import  { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AuthCallBackPage = () => {
