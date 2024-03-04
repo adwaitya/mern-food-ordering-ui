79c9a4d8193d6a9e6127119f2ed319ff7cf84c4f
adwaitya <adwaitya.118038@gmail.com>
2024-03-04 20:43:58 +0530

    Create restaurant form request

---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,195 @@
+import { Order, Restaurant } from "@/types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation, useQuery } from "react-query";
+import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getRestaurantRequest = async (): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/restaurant`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+    return response.json();
+  };
+
+  const { data: restaurant, isLoading } = useQuery(
+    "fetchRestaurant",
+    getRestaurantRequest
+  );
+
+  return { restaurant, isLoading };
+};
+
+export const useCreateRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const createRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+    console.log({restaurantFormData})
+    const response = await fetch(`${API_BASE_URL}/api/restaurant`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create restaurant");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutate: createRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(createRestaurantRequest);
+
+  if (isSuccess) {
+    toast.success("Restaurant created!");
+  }
+
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
+
+  return { createRestaurant, isLoading };
+};
+
+export const useUpdateRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/restaurant`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
+
+    if (!response) {
+      throw new Error("Failed to update restaurant");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutate: updateRestaurant,
+    isLoading,
+    error,
+    isSuccess,
+  } = useMutation(updateRestaurantRequest);
+
+  if (isSuccess) {
+    toast.success("Restaurant Updated");
+  }
+
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
+
+  return { updateRestaurant, isLoading };
+};
+
+export const useGetRestaurantOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getRestaurantOrdersRequest = async (): Promise<Order[]> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/restaurant/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch orders");
+    }
+
+    return response.json();
+  };
+
+  const { data: orders, isLoading } = useQuery(
+    "fetchMyRestaurantOrders",
+    getRestaurantOrdersRequest
+  );
+
+  return { orders, isLoading };
+};
+
+type UpdateOrderStatusRequest = {
+  orderId: string;
+  status: string;
+};
+
+export const useUpdateRestaurantOrder = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateRestaurantOrder = async (
+    updateStatusOrderRequest: UpdateOrderStatusRequest
+  ) => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/order/${updateStatusOrderRequest.orderId}/status`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status: updateStatusOrderRequest.status }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to update status");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutateAsync: updateRestaurantStatus,
+    isLoading,
+    isError,
+    isSuccess,
+    reset,
+  } = useMutation(updateRestaurantOrder);
+
+  if (isSuccess) {
+    toast.success("Order updated");
+  }
+
+  if (isError) {
+    toast.error("Unable to update order");
+    reset();
+  }
+
+  return { updateRestaurantStatus, isLoading };
+};
--- src/pages/ManagementRestaurantPage.tsx
+++ src/pages/ManagementRestaurantPage.tsx
@@ -1,12 +1,39 @@
+import { useCreateRestaurant, useGetRestaurant, useUpdateRestaurant } from "@/api/RestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm"
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@radix-ui/react-tabs";
 
 
 const ManagementRestaurantPage = () => {
-const onSave = () => {
+  const { createRestaurant, isLoading: isCreateLoading } =
+  useCreateRestaurant();
+// const { restaurant } = useGetRestaurant();
+const { updateRestaurant, isLoading: isUpdateLoading } =
+  useUpdateRestaurant();
 
-}
+  // const isEditing = !!restaurant;
   return (
-    <ManageRestaurantForm onSave={onSave} isLoading={false}/>
+    <Tabs defaultValue="manage-restaurant">
+    <TabsList>
+      <TabsTrigger value="orders">Orders</TabsTrigger>
+      <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+    </TabsList>
+    {/* <TabsContent
+      value="orders"
+      className="space-y-5 bg-gray-50 p-10 rounded-lg"
+    >
+      <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+      {orders?.map((order) => (
+        <OrderItemCard order={order} />
+      ))}
+    </TabsContent> */}
+    <TabsContent value="manage-restaurant">
+      <ManageRestaurantForm
+        // restaurant={restaurant}
+        onSave={createRestaurant}
+        isLoading={isCreateLoading || isUpdateLoading}
+      />
+    </TabsContent>
+  </Tabs>
   )
 }
 
